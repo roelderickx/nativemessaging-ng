bd4ce8351e936aaf74a6c9ac79fd4b9b71b53194
roelderickx <roel.derickx@gmail.com>
2024-01-06 13:18:24 +0100

    Increment version to 1.1.0

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(name="nativemessaging-ng",
-      version="1.0.1",
+      version="1.1.0",
       description="A package with basic native messaging apis for webextensions",
       long_description=long_description,
       long_description_content_type="text/markdown",
