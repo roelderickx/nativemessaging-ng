24877ffb3f6a23ef912900087a1531dccedda5a1
roelderickx <roel.derickx@gmail.com>
2024-01-06 14:59:13 +0100

    Move version number to version.py

--- setup.py
+++ setup.py
@@ -1,26 +1,31 @@
-#!/usr/bin/env python3
-from setuptools import setup
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
+import setuptools
 
-setup(name="nativemessaging-ng",
-      version="1.1.0",
-      description="A package with basic native messaging apis for webextensions",
-      long_description=long_description,
-      long_description_content_type="text/markdown",
-      url="https://github.com/roelderickx/nativemessaging-ng",
-      author="Rayquaza01, roelderickx",
-      author_email="nativemessaging-ng.pypi@derickx.be",
-      license="MPL 2.0",
-      packages=["nativemessaging"],
-      entry_points={
-          "console_scripts": ["nativemessaging-install = nativemessaging.install:main"]
-      },
-      include_package_data=True,
-      package_data={"": ["README.md"]},
-      classifiers=[
-          "Programming Language :: Python :: 3",
-          "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
-          "Operating System :: OS Independent"
-      ])
+exec(open("nativemessaging/version.py").read())
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    README = fh.read()
+
+setuptools.setup(
+    name="nativemessaging-ng",
+    version=__version__,
+    license=__license__,
+    author=__author__,
+    author_email="nativemessaging-ng.pypi@derickx.be",
+    description="A package with basic native messaging apis for webextensions",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/roelderickx/nativemessaging-ng",
+    packages=setuptools.find_packages(),
+    python_requires='>=3.0',
+    entry_points={
+        "console_scripts": ["nativemessaging-install = nativemessaging.install:main"]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Mozilla Public License 2.0 (MPL 2.0)",
+        "Operating System :: OS Independent"
+    ]
+)
