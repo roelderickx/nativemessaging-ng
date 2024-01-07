6c3551823d4e9cdf659ea80962e32647f76e9e5b
roelderickx <roel.derickx@gmail.com>
2024-01-07 06:54:40 +0100

    Increment version to 1.3.0

--- nativemessaging/version.py
+++ nativemessaging/version.py
@@ -1,14 +1,5 @@
-'''
-Copyright (c) 2012-2021 Roel Derickx, Paul Norman <penorman@mac.com>,
-Sebastiaan Couwenberg <sebastic@xs4all.nl>, The University of Vermont
-<andrew.guertin@uvm.edu>, github contributors
-
-Released under the MIT license, as given in the file LICENSE, which must
-accompany any distribution of this code.
-'''
-
 __author__ = "Rayquaza01, Roel Derickx"
 __program__ = "nativemessaging-ng"
-__version__ = "1.2.0"
+__version__ = "1.3.0"
 __license__ = "MPL 2.0 License"
 
